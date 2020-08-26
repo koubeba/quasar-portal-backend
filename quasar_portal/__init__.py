e80efa3306534bdded56ef54f98efa35288cdc63
Maja Jabłońska <majajjablonska@gmail.com>
2020-08-26 17:01:31 +0200

    Change library to pykafka

--- quasar_portal/__init__.py
+++ quasar_portal/__init__.py
@@ -11,7 +11,8 @@ def create_app() -> Flask:
     app: Flask = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=secrets.token_urlsafe(32),
-        KAFKA_BOOTSTRAP_SERVERS='35.241.252.88:9092'
+        KAFKA_BOOTSTRAP_SERVERS='35.241.252.88:9092',
+        ZOOKEEPER_SERVERS='35.241.252.88:2181'
     )
 
     from . import kafka_connector
@@ -20,17 +21,31 @@ def create_app() -> Flask:
     @app.before_first_request
     def create_kafka_context():
         global kafka_context
-        kafka_context = KafkaContext(bootstrap_servers=app.config['KAFKA_BOOTSTRAP_SERVERS'])
+        kafka_context = KafkaContext(bootstrap_servers=app.config['KAFKA_BOOTSTRAP_SERVERS'],
+                                     zookeeper_servers=app.config['ZOOKEEPER_SERVERS'])
 
     @app.route('/')
     def index():
-        return 'Quasar Portal Backend'
+        return {'msg': 'Quasar Portal Backend'}
 
-    @app.route('/connected')
-    def connected():
-        if kafka_context.producer_connected():
-            return 'Connected'
-        else:
-            return 'Not Connected'
+    @app.route('/brokers')
+    def brokers():
+        return str(kafka_context.get_brokers())
+
+    @app.route('/topics')
+    def topics():
+        return str(kafka_context.get_topics())
+
+    # @app.route('/connected')
+    # def connected():
+    #     if kafka_context.producer_connected():
+    #         return 'Connected'
+    #     else:
+    #         return 'Not Connected'
+
+    @app.route('/send_message')
+    def send_message():
+        kafka_context.send_message(b'hello!!!')
+        return 'sent!'
 
     return app
--- quasar_portal/kafka_context.py
+++ quasar_portal/kafka_context.py
@@ -1,25 +1,42 @@
-from kafka import KafkaProducer
-from typing import Optional
+from pykafka.cluster import Cluster
+from pykafka.handlers import ThreadingHandler
+from typing import List, Optional
 
 import json
 
 
 class KafkaContext:
-    def __init__(self, bootstrap_servers: str):
-        print(f'Creating Kafka Context with bootstrap servers {bootstrap_servers}...')
+    def __init__(self, bootstrap_servers: str, zookeeper_servers: str):
+        print(f'Creating Kafka Context with bootstrap servers {bootstrap_servers} and zookeeper servers {zookeeper_servers}...')
         self.__bootstrap_servers = bootstrap_servers
-        self.__producer__: Optional[KafkaProducer] = None
+        self.__zookeper_servers = zookeeper_servers
+        self.__client__: Optional[Cluster] = None
 
     @property
-    def producer(self) -> KafkaProducer:
-        if not self.__producer__:
-            self.__producer__ = self.__create_kafka_producer()
-        return self.__producer__
+    def client(self) -> Cluster:
+        if not self.__client__:
+            self.__client__ = self.__create_kafka_client()
+        return self.__client__
 
-    def producer_connected(self) -> bool:
-        return self.producer.bootstrap_connected()
+    def client_connected(self) -> bool:
+        pass
 
-    def __create_kafka_producer(self) -> KafkaProducer:
+    def get_brokers(self) -> List[str]:
+        return self.client.brokers
+
+    def get_topics(self) -> List[str]:
+        return self.client.topics
+
+    def send_message(self, message):
+        topics = self.client.topics
+        topic = topics.__getitem__(b'test')
+        print(topic.name)
+        print(topic.latest_available_offsets())
+        with topic.get_sync_producer() as producer:
+            producer.produce(message)
+
+    def __create_kafka_client(self) -> Cluster:
         print(f'Creating a Kafka Producer with bootstrap servers {self.__bootstrap_servers}')
-        return KafkaProducer(bootstrap_servers=self.__bootstrap_servers,
-                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+        return Cluster(hosts=self.__bootstrap_servers,
+                       handler=ThreadingHandler(),
+                       zookeeper_hosts=self.__zookeper_servers)
