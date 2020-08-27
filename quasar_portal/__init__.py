1ba4abf779691ba610527425cce3216e9ee56e07
Maja Jabłońska <majajjablonska@gmail.com>
2020-08-27 20:16:26 +0200

    Add file fetcher

--- quasar_portal/__init__.py
+++ quasar_portal/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_cors import CORS
 from typing import Any, Optional
 from .kafka_context import KafkaContext
 import json
@@ -13,9 +14,10 @@ def create_app() -> Flask:
     app: Flask = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=secrets.token_urlsafe(32),
-        KAFKA_BOOTSTRAP_SERVERS='35.241.252.88:9092,34.78.25.50:9092,35.233.58.29:9092',
-        ZOOKEEPER_SERVERS='35.241.252.88:2181'
+        KAFKA_BOOTSTRAP_SERVERS='35.187.31.167:9092,34.78.25.50:9092,35.233.58.29:9092',
+        ZOOKEEPER_SERVERS='35.187.31.167:2181'
     )
+    CORS(app)
 
     from . import kafka_connector
     kafka_connector.init_app(app)
@@ -39,6 +41,14 @@ def create_app() -> Flask:
             }
         }
 
+    @app.route('/get_messages_offset', methods=['GET'])
+    def get_messages_offset():
+        return {
+            'data': {
+                'offset': kafka_context.get_last_messages_offset()
+            }
+        }
+
     @app.route('/get_messages', methods=['GET'])
     def get_messages():
         messages_count: int = request.args.get('count', default=10, type=int)
@@ -48,11 +58,37 @@ def create_app() -> Flask:
             }
         }
 
+    @app.route('/get_sent_files_offset', methods=['GET'])
+    def get_files_offset():
+        return {
+            'data': {
+                'offset': kafka_context.get_last_sent_files_offset()
+            }
+        }
+
+    @app.route('/get_sent_files', methods=['GET'])
+    def get_sent_files():
+        messages_count: int = request.args.get('count', default=10, type=int)
+        return {
+            'data': {
+                'messages': kafka_context.get_last_sent_files(n=messages_count)
+            }
+        }
+
     @app.route('/send_message', methods=['POST'])
     def send_message():
         message: Optional[Any] = request.get_json()
         if message:
             kafka_context.send_message(json.dumps(message).encode(ENCODING))
+            return 'Sent!'
+        else:
+            return 'No message was provided'
+
+    @app.route('/send_file_data', methods=['POST'])
+    def send_file_data():
+        message: Optional[Any] = request.get_json()
+        if message:
+            kafka_context.send_file_data(json.dumps(message).encode(ENCODING))
             return 'Sent!'
         else:
             return 'No message was provided'
--- quasar_portal/kafka_context.py
+++ quasar_portal/kafka_context.py
@@ -30,30 +30,57 @@ class KafkaContext:
                 connected += 1
         return connected
 
+    def send_file_data(self, message):
+        # TODO: validate file data
+        topics = self.client.topics
+        topic: Topic = topics['sent_files']
+        with topic.get_sync_producer() as producer:
+            producer.produce(message)
+
     def send_message(self, message):
         topics = self.client.topics
         topic: Topic = topics['test']
         with topic.get_sync_producer() as producer:
             producer.produce(message)
 
-    def get_last_messages(self, n: int) -> Dict[int, str]:
+    def get_last_messages_offset(self, topic_name: str = 'test') -> int:
         topics = self.client.topics
-        topic: Topic = topics['test']
+        topic: Topic = topics[topic_name]
         consumer: BalancedConsumer = topic.get_balanced_consumer(consumer_group=b'portal',
                                                                  auto_offset_reset=OffsetType.LATEST,
                                                                  reset_offset_on_start=True)
-        n = min(max(consumer.held_offsets.values())+1, n)
-        partitions: List[Partition] = consumer.partitions
-        offsets = [(p, op - n)
-                   for p, op in consumer.held_offsets.items()]
-        # if we want to rewind before the beginning of the partition, limit to beginning
-        offsets = [(partitions[p], (o if o > -1 else -2)) for p, o in offsets]
-        # reset the consumer's offsets
-        consumer.reset_offsets(offsets)
+        partition_names: List[str] = list(consumer.partitions.keys())
+        if partition_names:
+            partition: Partition = consumer.partitions.__getitem__(partition_names[0])
+            return partition.latest_available_offset()
+        return 0
+
+    def get_last_messages(self, n: int, topic_name: str = 'test') -> Dict[int, str]:
+        topics = self.client.topics
+        topic: Topic = topics[topic_name]
+        consumer: BalancedConsumer = topic.get_balanced_consumer(consumer_group=b'portal',
+                                                                 auto_offset_reset=OffsetType.LATEST,
+                                                                 reset_offset_on_start=True)
+
+        if consumer.held_offsets:
+            n = min(max(consumer.held_offsets.values())+1, n)
+            partitions: List[Partition] = consumer.partitions
+            offsets = [(p, op - n)
+                       for p, op in consumer.held_offsets.items()]
+            # if we want to rewind before the beginning of the partition, limit to beginning
+            offsets = [(partitions[p], (o if o > -1 else -2)) for p, o in offsets]
+            # reset the consumer's offsets
+            consumer.reset_offsets(offsets)
         result: Dict[int, str] = {}
         for message in islice(consumer, n):
             result[int(message.offset)] = message.value.decode('utf-8')
         return result
+
+    def get_last_sent_files_offset(self) -> int:
+        return self.get_last_messages_offset(topic_name='sent_files')
+
+    def get_last_sent_files(self, n: int) -> Dict[int, str]:
+        return self.get_last_messages(n, topic_name='sent_files')
 
     def __create_kafka_client(self) -> Cluster:
         print(f'Creating a Kafka Producer with bootstrap servers {self.__bootstrap_servers}')
